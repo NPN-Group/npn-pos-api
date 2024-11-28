fac166ccc0f5b99ba61078a5c3ac5b8c6142044b
Prataad <pondaeroz008y@gmail.com>
2024-11-29 01:36:47 +0700

    feat: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
