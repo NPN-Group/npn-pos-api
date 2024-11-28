d9f5bf77174b9dbb1e20de5893072ed13b38152a
Prataad <pondaeroz008y@gmail.com>
2024-11-29 01:45:06 +0700

    feat: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
