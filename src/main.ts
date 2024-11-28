0c96c5bdfb0b72d1703c9aae53b4e47b8fb20ff4
Prataad <pondaeroz008y@gmail.com>
2024-11-29 01:40:27 +0700

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    stopAtFirstError: true,
+  }));
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
